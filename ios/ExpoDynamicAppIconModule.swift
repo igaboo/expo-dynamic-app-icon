05b7ca418ea9af29734b4fa89df99e6c8b2699fc
igaboo <jadenwatsond@gmail.com>
2024-11-14 03:03:17 -0800

    added ios alert on icon change

--- ios/ExpoDynamicAppIconModule.swift
+++ ios/ExpoDynamicAppIconModule.swift
@@ -6,7 +6,7 @@ public class ExpoDynamicAppIconModule: Module {
     Name("ExpoDynamicAppIcon")
 
     Function("setAppIcon") { (name:String) -> String in
-      self.setAppIconWithoutAlert(name)
+      self.setAppIcon(name)
 
       return name
     }
@@ -16,16 +16,9 @@ public class ExpoDynamicAppIconModule: Module {
     }
   }
 
-  private func setAppIconWithoutAlert(_ iconName: String?) {
+  private func setAppIcon(_ iconName: String?) {
     if UIApplication.shared.responds(to: #selector(getter: UIApplication.supportsAlternateIcons)) && UIApplication.shared.supportsAlternateIcons {
-      typealias setAlternateIconName = @convention(c) (NSObject, Selector, NSString?, @escaping (NSError) -> ()) -> ()
-      
-      let selectorString = "_setAlternateIconName:completionHandler:"
-      
-      let selector = NSSelectorFromString(selectorString)
-      let imp = UIApplication.shared.method(for: selector)
-      let method = unsafeBitCast(imp, to: setAlternateIconName.self)
-      method(UIApplication.shared, selector, iconName as NSString?, { _ in })
+      UIApplication.shared.setAlternateIconName(iconName)
     }
   }
 
